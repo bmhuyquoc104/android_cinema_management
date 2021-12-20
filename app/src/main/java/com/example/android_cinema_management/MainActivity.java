8daacfba440d80ee7a08e483727db429fd91bb6e
[bmhuyquoc104] <[s3823236@rmit.edu.vn]>
2021-12-20 14:00:29 +0700

    Add Logo and customize top bar

--- app/src/main/java/com/example/android_cinema_management/MainActivity.java
+++ app/src/main/java/com/example/android_cinema_management/MainActivity.java
@@ -2,15 +2,44 @@ package com.example.android_cinema_management;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Declare login and register button
+    Button loginAndRegister;
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Binding button with xml layout
+        loginAndRegister = findViewById(R.id.loginAndRegister);
+
+        // Create spannalbe String
+        SpannableStringBuilder builder = new SpannableStringBuilder();
+
+        // Text and color for string 1
+        SpannableString str1= new SpannableString("Login / ");
+        str1.setSpan(new ForegroundColorSpan(Color.rgb(161,161,161)), 0, str1.length(), 0);
+        builder.append(str1);
+
+        // Text and color for string 2
+        SpannableString str2= new SpannableString("Sign Up");
+        str2.setSpan(new ForegroundColorSpan(Color.rgb(222,22,25)), 0, str2.length(), 0);
+        builder.append(str2);
+
+        // Set text for button
+        loginAndRegister.setText( builder, Button.BufferType.SPANNABLE);
     }
 }
