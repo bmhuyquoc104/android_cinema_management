02bb3c8bedf3775c4e299efb82e3bb27bc994976
[bmhuyquoc104] <[s3823236@rmit.edu.vn]>
2021-12-25 21:38:06 +0700

    redesign UI page for login

--- app/src/main/java/com/example/android_cinema_management/Home.java
+++ app/src/main/java/com/example/android_cinema_management/Home.java
@@ -1,11 +1,15 @@
 package com.example.android_cinema_management;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +28,8 @@ public class Home extends Fragment {
     TabLayout layout;
     ViewPager2 viewpager2;
     HomeAdapter adapter;
+    //Declare login and register button
+    Button loginAndRegister;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -71,6 +77,7 @@ public class Home extends Fragment {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         layout = view.findViewById(R.id.mh_tab_layout);
         viewpager2 = view.findViewById(R.id.mh_viewpager2);
+        loginAndRegister = view.findViewById(R.id.loginAndRegister);
         // Initialize fragment manager
         FragmentManager fm = getParentFragmentManager();
         // Initialize adapter
@@ -101,6 +108,21 @@ public class Home extends Fragment {
             }
         });
 
+        // Create spannalbe String
+        SpannableStringBuilder builder = new SpannableStringBuilder();
+
+        // Text and color for string 1
+        SpannableString str1= new SpannableString("Login / ");
+        str1.setSpan(new ForegroundColorSpan(Color.rgb(161,161,161)), 0, str1.length(), 0);
+        builder.append(str1);
+
+        // Text and color for string 2
+        SpannableString str2= new SpannableString("Sign Up");
+        str2.setSpan(new ForegroundColorSpan(Color.rgb(222,22,25)), 0, str2.length(), 0);
+        builder.append(str2);
+
+        // Set text for button
+        loginAndRegister.setText( builder, Button.BufferType.SPANNABLE);
         return view;
     }
 }
