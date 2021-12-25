a25c100ee0c7c08fac43c2da095ba3bcca86b18c
IuKim <s3805504@rmit.edu.vn>
2021-12-25 18:34:54 +0700

    footer

---
+++ app/src/main/java/com/example/android_cinema_management/Component/Footer.java
@@ -0,0 +1,31 @@
+package com.example.android_cinema_management.Component;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.example.android_cinema_management.R;
+public class Footer extends Fragment {
+    public static Footer newInstance() {
+        Footer fragment = new Footer();
+        Bundle args = new Bundle();
+        return fragment;
+    }
+
+    public Footer() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_footer, container, false);
+    }
+}
