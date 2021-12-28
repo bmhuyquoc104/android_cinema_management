27e35ebf55bc1da8325c84af33d859c3678d66b4
bmhuyquoc104 <56634994+bmhuyquoc104@users.noreply.github.com>
2021-12-28 12:36:09 +0700

    create cinema model class to store information of cinema

--- app/src/main/java/com/example/android_cinema_management/Model/Cinema.java
+++ app/src/main/java/com/example/android_cinema_management/Model/Cinema.java
@@ -6,15 +6,22 @@ public class Cinema {
     private String address;
     private String latitude;
     private String longitude;
-
+    private String contactNumber;
+    private String imageUrl;
+    private String locationName;
+    private String cinemaId;
     public Cinema() {
     }
 
-    public Cinema(String name, String address, String latitude, String longitude) {
+    public Cinema(String cinemaId,String name, String address, String latitude, String longitude,String contactNumber, String imageUrl, String locationName) {
         this.name = name;
         this.address = address;
         this.latitude = latitude;
         this.longitude = longitude;
+        this.contactNumber = contactNumber;
+        this.imageUrl = imageUrl;
+        this.locationName = locationName;
+        this.cinemaId = cinemaId;
     }
 
     public String getName() {
@@ -49,4 +56,49 @@ public class Cinema {
         this.longitude = longitude;
     }
 
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+
+    public String getCinemaId() {
+        return cinemaId;
+    }
+
+    public void setCinemaId(String cinemaId) {
+        this.cinemaId = cinemaId;
+    }
+
+    @Override
+    public String toString() {
+        return "Cinema{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", latitude='" + latitude + '\'' +
+                ", longitude='" + longitude + '\'' +
+                ", contactNumber='" + contactNumber + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", locationName='" + locationName + '\'' +
+                ", cinemaId='" + cinemaId + '\'' +
+                '}';
+    }
 }
