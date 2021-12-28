c5cec8783939ffa40e9f490e18f8181294319df1
IuKim <s3805504@rmit.edu.vn>
2021-12-28 17:14:51 +0700

    Discount created

--- app/src/main/java/com/example/android_cinema_management/DiscountAndTicket.java
+++ app/src/main/java/com/example/android_cinema_management/DiscountAndTicket.java
@@ -3,62 +3,58 @@ package com.example.android_cinema_management;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DiscountAndTicket#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.android_cinema_management.Adapter.DiscountAdapter;
+import com.example.android_cinema_management.Model.Discount;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DiscountAndTicket extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private RecyclerView Discount;
+    private DiscountAdapter discountAdapter;
 
     public DiscountAndTicket() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DiscountAndTicket.
-     */
-    // TODO: Rename and change types and number of parameters
     public static DiscountAndTicket newInstance(String param1, String param2) {
         DiscountAndTicket fragment = new DiscountAndTicket();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_discount_and_ticket, container, false);
+        View view = inflater.inflate(R.layout.fragment_discount_and_ticket, container, false);
+        Discount = view.findViewById(R.id.discount);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(),2,GridLayoutManager.VERTICAL,false);
+        Discount.setLayoutManager(gridLayoutManager);
+
+        discountAdapter = new DiscountAdapter(getListDiscount());
+        Discount.setAdapter(discountAdapter);
+        return view;
+    }
+
+    private List<com.example.android_cinema_management.Model.Discount> getListDiscount() {
+        List<Discount> list = new ArrayList<>();
+        list.add(new Discount("Black Friday", "23/11/21-26/11/21"));
+        list.add(new Discount("Anniversary", "19/11/21-21/11/21"));
+        list.add(new Discount("Big Sale", "2/2/22-4/2/22"));
+        list.add(new Discount("New Year", "1/1/22"));
+
+        return list;
     }
 }
