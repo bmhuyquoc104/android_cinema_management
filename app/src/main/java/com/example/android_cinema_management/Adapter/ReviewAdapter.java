d342e023f0826d619de002798c296b8fbca9b504
Kendrick2000 <kendrick.truongphu2000@gmail.com>
2022-01-06 21:32:19 +0700

    finish adapter for review

--- app/src/main/java/com/example/android_cinema_management/Adapter/ReviewAdapter.java
+++ app/src/main/java/com/example/android_cinema_management/Adapter/ReviewAdapter.java
@@ -5,26 +5,53 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.android_cinema_management.Model.Review;
 import com.example.android_cinema_management.Model.User;
 import com.example.android_cinema_management.R;
 
 import java.util.ArrayList;
 
-public class ReviewAdapter extends RecyclerView.Adapter {
+public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.MyViewHolder> {
 
-    Context context;
-    ArrayList<User> userArrayList;
+    private Context context;
 
-    public static class CustomViewHolder extends RecyclerView.ViewHolder{
+    private ArrayList<Review> reviewArrayList;
+
+    public ReviewAdapter(Context context, ArrayList<Review> reviewArrayList) {
+        this.context = context;
+        this.reviewArrayList = reviewArrayList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.review_item,parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.userName.setText(reviewArrayList.get(position).getUser().get("fullName").toString());
+        holder.movieName.setText(reviewArrayList.get(position).getMovieName());
+        holder.movieRating.setText(reviewArrayList.get(position).getRateMovie());
+        holder.reviewContent.setText(reviewArrayList.get(position).getReviewContent());
+    }
+
+    @Override
+    public int getItemCount() {
+        return reviewArrayList.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
         ImageButton likeBtn, dislikeBtn;
         TextView userName, movieName, movieRating, reviewContent;
-
-        public CustomViewHolder(@NonNull View itemView) {
+        public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             likeBtn = itemView.findViewById(R.id.likeButton);
             dislikeBtn = itemView.findViewById(R.id.dislikeButton);
@@ -33,33 +60,5 @@ public class ReviewAdapter extends RecyclerView.Adapter {
             movieRating = itemView.findViewById(R.id.displayMovieRating);
             reviewContent = itemView.findViewById(R.id.review);
         }
-    }
-
-    public ReviewAdapter(Context context, ArrayList<User> userArrayList) {
-        this.context = context;
-        this.userArrayList = userArrayList;
-    }
-
-    @NonNull
-    @Override
-    public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(context).inflate(R.layout.review_item, parent, false);
-        return new CustomViewHolder(v);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
-    }
-
-//    @Override
-//    public void onBindViewHolder(@NonNull CustomViewHolder holder, int position) {
-//        User user = userArrayList.get(position);
-////        holder.likeBtn
-//    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
     }
 }
