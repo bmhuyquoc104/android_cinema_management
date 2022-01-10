1b8631a72918459607720a5ad3551a3d6899c45b
[bmhuyquoc104] <[s3823236@rmit.edu.vn]>
2022-01-10 21:25:31 +0700

    add review

---
+++ app/src/main/java/com/example/android_cinema_management/UserManagement/AddReviewFragment.java
@@ -0,0 +1,28 @@
+package com.example.android_cinema_management.UserManagement;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android_cinema_management.R;
+
+
+public class AddReviewFragment extends Fragment {
+
+
+
+    public AddReviewFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_review, container, false);
+    }
+}
