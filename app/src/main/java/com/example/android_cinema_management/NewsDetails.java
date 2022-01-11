914c308b0ec17e9385667bb8f906f4d6b92505bb
IuKim <s3805504@rmit.edu.vn>
2022-01-12 02:48:27 +0700

    semi finished news (without db for now) and added tabulation for TAndD

---
+++ app/src/main/java/com/example/android_cinema_management/NewsDetails.java
@@ -0,0 +1,52 @@
+package com.example.android_cinema_management;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.android_cinema_management.Model.News;
+
+public class NewsDetails extends AppCompatActivity {
+    private TextView NewsName, NewsDate, NewsDetail;
+    private ImageView Back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_details);
+
+//        Get the extra information to display
+        Bundle bundle = getIntent().getExtras();
+        if (bundle == null) {
+            return;
+        }
+
+        News news = (News) bundle.get("object_news");
+
+        NewsName = findViewById(R.id.newsName);
+        NewsDate = findViewById(R.id.newsDate);
+        NewsDetail = findViewById(R.id.newsDetail);
+
+//        The back button
+        Back = findViewById(R.id.back);
+        Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+
+//        Setting the information into the views
+        String name = news.getName();
+        String date = news.getMonth();
+        String content = news.getContent();
+
+        NewsName.setText(getBaseContext().getString(R.string.discountName, name));
+        NewsDate.setText(getBaseContext().getString(R.string.discountDate, date));
+        NewsDetail.setText(content);
+    }
+}
--- app/src/main/java/com/example/android_cinema_management/NewsFragment.java
+++ app/src/main/java/com/example/android_cinema_management/NewsFragment.java
@@ -4,18 +4,32 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AutoCompleteTextView;
 import android.widget.ImageView;
 
 import com.example.android_cinema_management.AccountManagement.SignUp;
+import com.example.android_cinema_management.Adapter.DiscountAdapter;
+import com.example.android_cinema_management.Model.Discount;
+import com.example.android_cinema_management.Model.News;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
 
 
 public class NewsFragment extends Fragment {
     //Declare imageview
-    ImageView close;
+    ImageView Back;
+
+    private RecyclerView News;
+    private ArrayList<News> newsArrayList;
+    FirebaseFirestore db;
 
 
     public NewsFragment() {
@@ -35,10 +49,20 @@ public class NewsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_news, container, false);
-        close = view.findViewById(R.id.fragment_news_close_iv);
-        close.setOnClickListener(View ->{
+
+//        Back button
+        Back = view.findViewById(R.id.back);
+        Back.setOnClickListener(View ->{
             startActivity(new Intent(getContext(), MainActivity.class));
         });
+
+        News = view.findViewById(R.id.newsItems);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(),2,GridLayoutManager.VERTICAL,false);
+        News.setLayoutManager(gridLayoutManager);
+
+//        News database
+        db = FirebaseFirestore.getInstance();
+        newsArrayList = new ArrayList<News>();
         return view;
     }
 }
