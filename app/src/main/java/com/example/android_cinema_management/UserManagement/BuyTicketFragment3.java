c4efdb291724990fbfbf33253d6cfd1a7535d086
[bmhuyquoc104] <[s3823236@rmit.edu.vn]>
2022-01-12 17:05:00 +0700

    add comment for buy by ticket fragment 3

--- app/src/main/java/com/example/android_cinema_management/UserManagement/BuyTicketFragment3.java
+++ app/src/main/java/com/example/android_cinema_management/UserManagement/BuyTicketFragment3.java
@@ -36,8 +36,10 @@ public class BuyTicketFragment3 extends Fragment {
 
     //Declare boolean
     private boolean chooseSeat = false;
-    ViewGroup layout;
+    ViewGroup chooseSeatLayout;
 
+
+    // Declare the string of all seats
     String seats = "_AAAAAAAAAARRRR_/"
             + "_________________/"
             + "UU__AAAARRRRR__RR/"
@@ -49,20 +51,22 @@ public class BuyTicketFragment3 extends Fragment {
             + "AA__AARR_UUUU__RR/"
             + "AA__UUUU_UURR__RR/"
             + "_________________/"
-//            + "UU_AAAAAAAUUUU_RR/"
-//            + "RR_AAAAAAAAAAA_AA/"
-//            + "AA_UUAAAAAUUUU_AA/"
-//            + "AA_AAAAAAUUUUU_AA/"
             + "_________________/";
 
-    List<TextView> seatViewList = new ArrayList<>();
-    int seatSize = 100;
-    int seatGaping = 10;
+    // Declare list of textview for render seat programmatically
+    List<TextView> listOfSeatView = new ArrayList<>();
+    // Set the width for each seat
+    int seatWidth = 100;
+    // Set the margin between each seat
+    int seatMargin = 10;
 
-    int STATUS_AVAILABLE = 1;
-    int STATUS_UNAVAILABLE = 2;
-    int STATUS_RESERVED = 3;
-    String selectedIds = "";
+    // Set status of the seat
+    int SEAT_IS_AVAILABLE = 1;
+    int SEAT_IS_UNAVAILABLE = 2;
+    int SEAT_IS_RESERVED = 3;
+
+    // Declare the string
+    String seatChosen = "";
 
     public BuyTicketFragment3() {
         // Required empty public constructor
@@ -81,12 +85,13 @@ public class BuyTicketFragment3 extends Fragment {
         selecting = view.findViewById(R.id.buy_ticket_selecting_tv);
         reserved = view.findViewById(R.id.buy_ticket_reserved_tv);
         next3 = view.findViewById(R.id.buy_by_movie3_next_bt);
+        chooseSeatLayout = view.findViewById(R.id.layoutSeat);
         // Function to change fragment and send data to next step in buying ticket
 
         next3.setEnabled(false);
         next3.setOnClickListener(View -> {
             Bundle bundle = new Bundle();
-            bundle.putString("seat", selectedIds);
+            bundle.putString("seat", seatChosen);
             BuyTicketFragment4 fragment4 = new BuyTicketFragment4();
             fragment4.setArguments(bundle);
             FragmentManager fm = getParentFragmentManager();
@@ -96,19 +101,19 @@ public class BuyTicketFragment3 extends Fragment {
 
         });
 
-        layout = view.findViewById(R.id.layoutSeat);
+
 
         seats = "/" + seats;
-
         LinearLayout layoutSeat = new LinearLayout(getContext());
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         layoutSeat.setOrientation(LinearLayout.VERTICAL);
         layoutSeat.setLayoutParams(params);
-        layoutSeat.setPadding(8 * seatGaping, 8 * seatGaping, 8 * seatGaping, 8 * seatGaping);
-        layout.addView(layoutSeat);
+        layoutSeat.setPadding(8 * seatMargin, 8 * seatMargin, 8 * seatMargin, 8 * seatMargin);
+        chooseSeatLayout.addView(layoutSeat);
 
         LinearLayout layout = null;
 
+        // Initialize count
         int count = 0;
 
         for (int index = 0; index < seats.length(); index++) {
@@ -116,67 +121,70 @@ public class BuyTicketFragment3 extends Fragment {
                 layout = new LinearLayout(getContext());
                 layout.setOrientation(LinearLayout.HORIZONTAL);
                 layoutSeat.addView(layout);
+                // Render appropriate seat if seat is unavailable
             } else if (seats.charAt(index) == 'U') {
                 count++;
                 TextView textView = new TextView(getContext());
-                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(seatSize, seatSize);
-                layoutParams.setMargins(seatGaping, seatGaping, seatGaping, seatGaping);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(seatWidth, seatWidth);
+                layoutParams.setMargins(seatMargin, seatMargin, seatMargin, seatMargin);
                 textView.setLayoutParams(layoutParams);
-                textView.setPadding(0, 0, 0, 2 * seatGaping);
+                textView.setPadding(0, 0, 0, 2 * seatMargin);
                 textView.setId(count);
                 textView.setGravity(Gravity.CENTER);
                 textView.setBackgroundResource(R.drawable.seat_unavailable);
                 textView.setTextColor(Color.BLACK);
-                textView.setTag(STATUS_UNAVAILABLE);
+                textView.setTag(SEAT_IS_UNAVAILABLE);
                 textView.setText(count + "");
                 textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 11);
                 layout.addView(textView);
-                seatViewList.add(textView);
+                listOfSeatView.add(textView);
                 textView.setOnClickListener(View -> {
                     chooseSeat(textView);
                 });
+                // Render appropriate seat if seat is available
             } else if (seats.charAt(index) == 'A') {
                 count++;
                 TextView textView = new TextView(getContext());
-                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(seatSize, seatSize);
-                layoutParams.setMargins(seatGaping, seatGaping, seatGaping, seatGaping);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(seatWidth, seatWidth);
+                layoutParams.setMargins(seatMargin, seatMargin, seatMargin, seatMargin);
                 textView.setLayoutParams(layoutParams);
-                textView.setPadding(0, 0, 0, 2 * seatGaping);
+                textView.setPadding(0, 0, 0, 2 * seatMargin);
                 textView.setId(count);
                 textView.setGravity(Gravity.CENTER);
                 textView.setBackgroundResource(R.drawable.seat_available);
                 textView.setText(count + "");
                 textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 11);
                 textView.setTextColor(Color.WHITE);
-                textView.setTag(STATUS_AVAILABLE);
+                textView.setTag(SEAT_IS_AVAILABLE);
                 layout.addView(textView);
-                seatViewList.add(textView);
+                listOfSeatView.add(textView);
                 textView.setOnClickListener(View -> {
                     chooseSeat(textView);
                 });
+                // Render appropriate seat if seat is reserved
             } else if (seats.charAt(index) == 'R') {
                 count++;
                 TextView textView = new TextView(getContext());
-                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(seatSize, seatSize);
-                layoutParams.setMargins(seatGaping, seatGaping, seatGaping, seatGaping);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(seatWidth, seatWidth);
+                layoutParams.setMargins(seatMargin, seatMargin, seatMargin, seatMargin);
                 textView.setLayoutParams(layoutParams);
-                textView.setPadding(0, 0, 0, 2 * seatGaping);
+                textView.setPadding(0, 0, 0, 2 * seatMargin);
                 textView.setId(count);
                 textView.setGravity(Gravity.CENTER);
                 textView.setBackgroundResource(R.drawable.seat_reserved);
                 textView.setText(count + "");
                 textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 11);
                 textView.setTextColor(Color.BLACK);
-                textView.setTag(STATUS_RESERVED);
+                textView.setTag(SEAT_IS_RESERVED);
                 layout.addView(textView);
-                seatViewList.add(textView);
+                listOfSeatView.add(textView);
                 textView.setOnClickListener(View -> {
                     chooseSeat(textView);
                 });
             } else if (seats.charAt(index) == '_') {
                 TextView textView = new TextView(getContext());
-                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(seatSize, seatSize);
-                layoutParams.setMargins(seatGaping, seatGaping, seatGaping, seatGaping);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(seatWidth, seatWidth);
+                layoutParams.setMargins(seatMargin, seatMargin, seatMargin, seatMargin);
                 textView.setLayoutParams(layoutParams);
                 textView.setBackgroundColor(Color.TRANSPARENT);
                 textView.setText("");
@@ -184,28 +192,35 @@ public class BuyTicketFragment3 extends Fragment {
             }
         }
 
-
+        // Resize the image of available seat
         @SuppressLint("UseCompatLoadingForDrawables") Drawable availableImage = getResources().getDrawable(R.drawable.seat_available);
         Bitmap bitmap = ((BitmapDrawable) availableImage).getBitmap();
         Drawable d = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(bitmap, 50, 50, true));
+        // Set the drawable Image programmatically for textview
         available.setCompoundDrawablesWithIntrinsicBounds(d,null,null,null);
         available.setText("Seat is Available");
 
+        // Resize the image of unavailable seat
         @SuppressLint("UseCompatLoadingForDrawables") Drawable unavailableImage = getResources().getDrawable(R.drawable.seat_unavailable);
         Bitmap bitmap2 = ((BitmapDrawable) unavailableImage).getBitmap();
         Drawable d2 = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(bitmap2, 50, 50, true));
+        // Set the drawable Image programmatically for textview
         unAvailable.setCompoundDrawablesWithIntrinsicBounds(d2,null,null,null);
         unAvailable.setText("Seat is Unavailable");
 
+        // Resize the image of selecting seat
         @SuppressLint("UseCompatLoadingForDrawables") Drawable selectingImage = getResources().getDrawable(R.drawable.seat_selecting2);
         Bitmap bitmap3 = ((BitmapDrawable) selectingImage).getBitmap();
         Drawable d3 = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(bitmap3, 50, 50, true));
+        // Set the drawable Image programmatically for textview
         selecting.setCompoundDrawablesWithIntrinsicBounds(d3,null,null,null);
         selecting.setText("Seat is Selecting");
 
+        // Resize the image of reserved seat
         @SuppressLint("UseCompatLoadingForDrawables") Drawable reservedImage = getResources().getDrawable(R.drawable.seat_reserved);
         Bitmap bitmap4 = ((BitmapDrawable) reservedImage).getBitmap();
         Drawable d4 = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(bitmap4, 50, 50, true));
+        // Set the drawable Image programmatically for textview
         reserved.setCompoundDrawablesWithIntrinsicBounds(d4,null,null,null);
         reserved.setText("Seat is Reserved");
 
@@ -213,15 +228,20 @@ public class BuyTicketFragment3 extends Fragment {
         return view;
     }
 
+    /*
+    *Function to check the seat that the user chose is whether available,unavailable or reserved
+    * */
     @SuppressLint("ResourceAsColor")
     private void chooseSeat (TextView view){
-        if ((int) view.getTag() == STATUS_AVAILABLE) {
-            if (selectedIds.contains(view.getId() + ",")) {
-                selectedIds = selectedIds.replace(+view.getId() + ",", "");
+        if ((int) view.getTag() == SEAT_IS_AVAILABLE) {
+            if (seatChosen.contains(view.getId() + ",")) {
+                seatChosen = seatChosen.replace(+view.getId() + ",", "");
                 view.setBackgroundResource(R.drawable.seat_available);
                 view.setTextColor(Color.WHITE);
+                // Announce the user the seat is unselected
                 Toast.makeText(getContext(), "Seat " + view.getId() + " is Unselected", Toast.LENGTH_SHORT).show();
                 chooseSeat = false;
+                // disable the next button
                 if(chooseSeat){
                     next3.setEnabled(true);
                 }
@@ -230,12 +250,14 @@ public class BuyTicketFragment3 extends Fragment {
                 }
 
             } else {
-                selectedIds = selectedIds + view.getId() + ",";
+                seatChosen = seatChosen + view.getId() + ",";
                 view.setBackgroundResource(R.drawable.seat_selecting2);
                 view.setTextColor(Color.rgb(222,22,25));
-                System.out.println("huy ne" + selectedIds);
+                System.out.println("huy ne" + seatChosen);
+                // Announce the user the seat is selecting
                 Toast.makeText(getContext(), "Seat " + view.getId() + " is Selecting", Toast.LENGTH_SHORT).show();
                 chooseSeat = true;
+                // enable the next button
                 if(chooseSeat){
                     next3.setEnabled(true);
                 }
@@ -243,9 +265,11 @@ public class BuyTicketFragment3 extends Fragment {
                     next3.setEnabled(false);
                 }
             }
-        } else if ((int) view.getTag() == STATUS_UNAVAILABLE) {
+        } else if ((int) view.getTag() == SEAT_IS_UNAVAILABLE) {
+            // Announce the user the seat is unavailable
             Toast.makeText(getContext(), "Seat " + view.getId() + " is Unavailable", Toast.LENGTH_SHORT).show();
-        } else if ((int) view.getTag() == STATUS_RESERVED) {
+        } else if ((int) view.getTag() == SEAT_IS_RESERVED) {
+            // Announce the user the seat is reserved
             Toast.makeText(getContext(), "Seat " + view.getId() + " is Reserved", Toast.LENGTH_SHORT).show();
         }
     }
