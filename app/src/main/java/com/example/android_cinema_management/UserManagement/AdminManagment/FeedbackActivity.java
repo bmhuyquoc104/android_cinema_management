cce279db779442ffe5cf672236ae9b86c7901a53
Kendrick2000 <56669029+Kendrick2000@users.noreply.github.com>
2022-01-14 13:24:55 +0700

    working on Feedback

--- app/src/main/java/com/example/android_cinema_management/UserManagement/AdminManagment/FeedbackActivity.java
+++ app/src/main/java/com/example/android_cinema_management/UserManagement/AdminManagment/FeedbackActivity.java
@@ -1,16 +1,72 @@
 package com.example.android_cinema_management.UserManagement.AdminManagment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.android_cinema_management.Adapter.FeedbackAdapter;
+import com.example.android_cinema_management.Model.Feedback;
 import com.example.android_cinema_management.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class FeedbackActivity extends AppCompatActivity {
 
+    //Declare recyclerview
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    //Declare adapter
+    private FeedbackAdapter feedbackAdapter;
+    //Declare Movie list
+    public static ArrayList<Feedback> feedbackArrayList;
+
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseUser user = firebaseAuth.getCurrentUser();
+    String userId = user.getUid();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+
+        feedbackArrayList = new ArrayList<>();
+        //call function get reviews data
+        getFeedbacks(db, feedbackArrayList, () -> {
+            System.out.println("FEEDBACK LIST: " + feedbackArrayList);
+
+            // Set fixed size for recycler view
+            recyclerView = findViewById(R.id.user_list_reviews_recycler_view);
+            recyclerView.setHasFixedSize(true);
+            feedbackAdapter = new FeedbackAdapter(this, feedbackArrayList);
+            layoutManager = new LinearLayoutManager(this);
+            recyclerView.setLayoutManager(layoutManager);
+            // Specify an adapter
+            recyclerView.setAdapter(feedbackAdapter);
+        });
+    }
+
+    //Function get reviews data
+    private void getFeedbacks(FirebaseFirestore db, ArrayList<Feedback> feedbackArrayList, Runnable callback) {
+        feedbackArrayList.clear();
+        db.collection("feedback").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                int count = 0;
+                for (DocumentSnapshot doc : Objects.requireNonNull(task.getResult())) {
+                    count++;
+                    Feedback dataContainer = doc.toObject(Feedback.class);
+                    feedbackArrayList.add(dataContainer);
+                    if (count == Objects.requireNonNull(task.getResult()).size()) {
+                        callback.run();
+                    }
+                }
+            }
+        });
     }
 }
