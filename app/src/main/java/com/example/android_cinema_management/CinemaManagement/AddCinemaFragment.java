546a6f4a2a7c92c698f3773ea163ee7deca82816
Kendrick2000 <56669029+Kendrick2000@users.noreply.github.com>
2022-01-14 08:54:10 +0700

    Khang 198: minor adjustment before transfer workflow

--- app/src/main/java/com/example/android_cinema_management/CinemaManagement/AddCinemaFragment.java
+++ app/src/main/java/com/example/android_cinema_management/CinemaManagement/AddCinemaFragment.java
@@ -12,19 +12,24 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.android_cinema_management.R;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 
 public class AddCinemaFragment extends Fragment {
 
-    EditText cinemaName, cinemaAddress, contactNumber, locationName, latitude, longitude, review, rate;
+    EditText cinemaName, cinemaAddress, contactNumber, locationName, latitude, longitude, review, rate, imageURL;
     Button addCinemaBtn;
     //Declare FirebaseFirestore FirebaseAuth FirebaseUser String
     FirebaseFirestore db;
@@ -60,8 +65,10 @@ public class AddCinemaFragment extends Fragment {
         locationName = view.findViewById(R.id.admin_add_cinema_location_name_tv);
         latitude = view.findViewById(R.id.admin_add_cinema_latitude_sent_et);
         longitude = view.findViewById(R.id.admin_add_cinema_longitude_sent_et);
+        city = view.findViewById(R.id.admin_add_cinema_city_text_layout);
         review = view.findViewById(R.id.admin_add_cinema_review_sent_et);
         rate = view.findViewById(R.id.admin_add_cinema_rate_sent_et);
+        imageURL = view.findViewById(R.id.admin_add_cinema_image_sent_et);
 
         addCinemaBtn = view.findViewById(R.id.admin_add_cinema_add_bt);
 
@@ -73,6 +80,29 @@ public class AddCinemaFragment extends Fragment {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 cityChosen = parent.getItemAtPosition(position).toString();
             }
+        });
+
+        addCinemaBtn.setOnClickListener(View -> {
+            String id = UUID.randomUUID().toString();
+            Map<String, Object> cinemaMap = new HashMap<>();
+            cinemaMap.put("id", id);
+            cinemaMap.put("name", cinemaName);
+            cinemaMap.put("address", cinemaAddress);
+            cinemaMap.put("contactNumber", contactNumber);
+            cinemaMap.put("locationName", locationName);
+            cinemaMap.put("latitude", latitude);
+            cinemaMap.put("longitude", longitude);
+            cinemaMap.put("city", city);
+            cinemaMap.put("review", review);
+            cinemaMap.put("rate", rate);
+            cinemaMap.put("imageURL", imageURL);
+
+            DocumentReference docRef = db.collection("Cinema").document(id);
+            docRef.set(cinemaMap).addOnCompleteListener(task -> {
+               if (task.isSuccessful()){
+                   Toast.makeText(getActivity(), "Cinema successfully addedd", Toast.LENGTH_SHORT).show();
+               }
+            });
         });
 
         return view;
