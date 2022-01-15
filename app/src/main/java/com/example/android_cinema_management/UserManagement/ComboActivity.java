2dfd5df1273ac4bd13759fd476ae447ab28cfbc1
[bmhuyquoc104] <[s3823236@rmit.edu.vn]>
2022-01-15 12:49:53 +0700

    add combo activity for user

---
+++ app/src/main/java/com/example/android_cinema_management/UserManagement/ComboActivity.java
@@ -0,0 +1,16 @@
+package com.example.android_cinema_management.UserManagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.android_cinema_management.R;
+
+public class ComboActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_combo2);
+    }
+}
