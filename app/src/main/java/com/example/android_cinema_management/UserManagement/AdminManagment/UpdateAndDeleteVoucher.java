cebe3c74b62a8b9b76ab63367e8545ae6d90a98c
Kendrick2000 <kendrick.truongphu2000@gmail.com>
2022-01-16 21:37:01 +0700

    finished voucher and add like dislike function button

--- app/src/main/java/com/example/android_cinema_management/UserManagement/AdminManagment/UpdateAndDeleteVoucher.java
+++ app/src/main/java/com/example/android_cinema_management/UserManagement/AdminManagment/UpdateAndDeleteVoucher.java
@@ -1,4 +1,4 @@
-package com.example.android_cinema_management.UserManagement;
+package com.example.android_cinema_management.UserManagement.AdminManagment;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +10,6 @@ import android.widget.Toast;
 
 import com.example.android_cinema_management.Model.Voucher;
 import com.example.android_cinema_management.R;
-import com.example.android_cinema_management.UserManagement.AdminManagment.UpdateAndDeleteCombo;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
